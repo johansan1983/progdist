03b62e11ebfea8b7f4a0d39eca1a6cde885b0830
Johan Sanchez <johansan@cotecnova.edu.co>
2026-05-12 21:49:34 -0500

    Sincronizacion de cambios

--- chat-service/target/classes/db/migration/V5__Add_DM_Unique_Constraint.sql
+++
@@ -1,4 +0,0 @@
--- Prevent duplicate DM conversations between the same pair of users
-CREATE UNIQUE INDEX IF NOT EXISTS uq_dm_pair
-    ON conversation (dm_participant_a, dm_participant_b)
-    WHERE type = 'DIRECT';
